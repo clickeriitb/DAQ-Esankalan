aff27e0a60b38578be43d0f43d2e6f1ee7838b8c
ankurgarg101 <ankurgarg101@gmail.com>
2014-06-26 02:24:19 +0530

    Adds I2C fragment display

---
+++ DAQ_App/src/com/idl/daq/I2C_ItemClass.java
@@ -0,0 +1,46 @@
+package com.idl.daq;
+
+public class I2C_ItemClass {
+
+	String type;
+	String addr;
+	String val;
+	int delay;
+
+	public I2C_ItemClass(){
+		type = "";
+		addr = "";
+		val = "";
+		delay = 1000;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setVal(String val) {
+		this.val = val;
+	}
+
+	public String getVal() {
+		return val;
+	}
+
+	public void setDelay(String delay) {
+		this.delay = Integer.parseInt(delay);
+	}
+	public int getDelay(){
+		return this.delay;
+	}
+}
