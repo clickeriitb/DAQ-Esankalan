bd3cae3dced5183a0c57d601b646142400048e49
Gaurav <gaurav.mittal.191013@gmail.com>
2014-06-30 19:39:20 +0530

    Makes modifications in parsing the sensor data received

--- DAQ_App/src/com/idl/daq/DetailsFrag.java
+++ DAQ_App/src/com/idl/daq/DetailsFrag.java
@@ -32,55 +32,56 @@ import com.daq.tabsswipe.adapter.TabsPagerAdapter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphViewSeries;
 
-public class DetailsFrag extends Fragment implements ActionBar.TabListener, LoaderCallbacks<Void>{
+public class DetailsFrag extends Fragment implements ActionBar.TabListener,
+		LoaderCallbacks<Void> {
 
 	private View rootView;
 	private TabHost tabHost;
 	private ViewPager viewPager;
-	//to take the tabs out of the action bar
+	// to take the tabs out of the action bar
 	private PagerTabStrip strip;
 	private ActionBar actionBar;
 	private TabsPagerAdapter mAdapter;
-	private String[] tabs = {"Graphical", "Tabular" };
-	//recieves sensor object from SensorListActivity in the form of the tabId 
+	private String[] tabs = { "Graphical", "Tabular" };
+	// recieves sensor object from SensorListActivity in the form of the tabId
 	private Sensor mySensor;
-	
+
 	private Context c;
 	private GlobalState gS;
-	
-	HashMap<String,Boolean> checkEmptyRegisters;
-	
-	//needed for display
-	private ArrayList<String> data,time,info;
+
+	// HashMap<String, Boolean> checkEmptyRegisters;
+
+	// needed for display
+	private ArrayList<String> data, time, info;
 	private ArrayAdapter<String> infoAdapter;
-	
+
 	int count;
-	//why
+	// why
 	static int countFrag = 0;
-	
+
 	ArrayList<JSONObject> t;
-	
-	//a new series is being built for each DetailsFrag
+
+	// a new series is being built for each DetailsFrag
 	public GraphViewSeries series;
 	private double TIME = 0;
-	
+
 	Callbacks sensorInfoCallbacks;
-	
-	public interface Callbacks{
-		
+
+	public interface Callbacks {
+
 		public ArrayAdapter<String> getArrayAdapter();
-		
+
 		public ArrayList<String> getArrayList();
-		
+
 		public GraphViewSeries getSeries();
 	}
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
 		setRetainInstance(true);
-		
+
 	}
 
 	@Override
@@ -106,17 +107,16 @@ public class DetailsFrag extends Fragment implements ActionBar.TabListener, Load
 				false);
 		viewPager = (ViewPager) rootView.findViewById(R.id.pager);
 		strip = (PagerTabStrip) rootView.findViewById(R.id.pager_tabs);
-		
+
 		actionBar = getActivity().getActionBar();
 		L.d("action bar selected");
 		mAdapter = new TabsPagerAdapter(getChildFragmentManager());
 		viewPager.setAdapter(mAdapter);
 		L.d("view pager set");
-//		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-//		setupTabs();
+		// actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		// setupTabs();
 		L.d("setup tabs");
-		
-		
+
 		/**
 		 * on swiping the viewpager make respective tab selected
 		 * */
@@ -126,8 +126,8 @@ public class DetailsFrag extends Fragment implements ActionBar.TabListener, Load
 			public void onPageSelected(int position) {
 				// on changing the page
 				// make respected tab selected
-//				actionBar.setSelectedNavigationItem(position);
-				L.d("action bar position: "+position);
+				// actionBar.setSelectedNavigationItem(position);
+				L.d("action bar position: " + position);
 			}
 
 			@Override
@@ -141,52 +141,52 @@ public class DetailsFrag extends Fragment implements ActionBar.TabListener, Load
 		data = new ArrayList<String>();
 		time = new ArrayList<String>();
 		count = 0;
-		//load the asyc task
+		// load the asyc task
 		getLoaderManager().initLoader(0, null, this);
-		checkEmptyRegisters = new HashMap<String,Boolean>();
-		initializeInputs();
-		
+		// checkEmptyRegisters = new HashMap<String, Boolean>();
+		// initializeInputs();
+
 		return rootView;
 	}
 
-	private void initializeInputs() {
-		// TODO Auto-generated method stub
-		if(mySensor instanceof I2CProc){
-			for(I2C_ItemClass i : ((I2CProc) mySensor).getExecList()){
-				checkEmptyRegisters.put(i.getAddr(),true);
-			}
-		}else{
-			checkEmptyRegisters.put("pin",true);
-		}
-	}
+	// private void initializeInputs() {
+	// // TODO Auto-generated method stub
+	// if (mySensor instanceof I2CProc) {
+	// for (I2C_ItemClass i : ((I2CProc) mySensor).getExecList()) {
+	// checkEmptyRegisters.put(i.getAddr(), true);
+	// }
+	// } else {
+	// checkEmptyRegisters.put("pin", true);
+	// }
+	// }
 
 	private void setupTabs() {
 		// TODO Auto-generated method stub
-		for(String tab : tabs){
+		for (String tab : tabs) {
 			actionBar.addTab(actionBar.newTab().setText(tab)
 					.setTabListener(this));
 		}
-		
+
 	}
 
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
 		viewPager.setCurrentItem(tab.getPosition());
-		L.d("tab position: "+tab.getPosition());
+		L.d("tab position: " + tab.getPosition());
 	}
 
 	@Override
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
-//		viewPager.setCurrentItem(tab.getPosition());
-//		L.d("tab position: "+tab.getPosition());
+		// viewPager.setCurrentItem(tab.getPosition());
+		// L.d("tab position: "+tab.getPosition());
 	}
 
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setSensor(Sensor sensor) {
@@ -200,17 +200,17 @@ public class DetailsFrag extends Fragment implements ActionBar.TabListener, Load
 		gS = (GlobalState) c;
 	}
 
-//	@Override
-//	public Context getContext() {
-//		// TODO Auto-generated method stub
-//		return c;
-//	}
-//
-//	@Override
-//	public Sensor getSensor() {
-//		// TODO Auto-generated method stub
-//		return mySensor;
-//	}
+	// @Override
+	// public Context getContext() {
+	// // TODO Auto-generated method stub
+	// return c;
+	// }
+	//
+	// @Override
+	// public Sensor getSensor() {
+	// // TODO Auto-generated method stub
+	// return mySensor;
+	// }
 
 	@Override
 	public Loader<Void> onCreateLoader(int arg0, Bundle arg1) {
@@ -243,7 +243,7 @@ public class DetailsFrag extends Fragment implements ActionBar.TabListener, Load
 
 	private void loadData() {
 		// TODO Auto-generated method stub
-		//it recieves the new data from SensorListActivity
+		// it receives the new data from SensorListActivity
 		info = sensorInfoCallbacks.getArrayList();
 		infoAdapter = sensorInfoCallbacks.getArrayAdapter();
 		L.d("received adapters");
@@ -251,60 +251,77 @@ public class DetailsFrag extends Fragment implements ActionBar.TabListener, Load
 		series = mAdapter.graph.series;
 		L.d("Received series");
 		t = gS.getTemp();
-		for(int i=data.size();i<t.size();++i){
+		for (int i = data.size(); i < t.size(); ++i) {
 			L.d("value of i " + i);
 			try {
-				if(t.get(i).get("sensor_code").equals(mySensor.getSensorName())){
-					for(Map.Entry<String, Formula> e : mySensor.getFormulaContainer().getFc().entrySet()){
-						L.d(e.getKey()+" "+e.getValue());
-					}
+				if (t.get(i).get("sensor_code")
+						.equals(mySensor.getSensorName())) {
+					// for(Map.Entry<String, Formula> e :
+					// mySensor.getFormulaContainer().getFc().entrySet()){
+					// L.d(e.getKey()+" "+e.getValue());
+					// }
 					String date = t.get(i).getString("date");
 					time.add(date);
-					String sensorData ="";
-					if(mySensor.getFormulaContainer()!=null){
+					String sensorData = "";
+					if (mySensor.getFormulaContainer() != null) {
 						Formula f;
-						if(mySensor instanceof I2CProc){
-							String dataList[] = t.get(i).getString("data").split(":");
-							String register = dataList[0];
-							double val = Double.parseDouble(dataList[1]);
-							f=null;
-							for(Map.Entry<String, Formula> e : mySensor.getFormulaContainer().getFc().entrySet()){
-								if(e.getValue().getDisplayName().equals(register)){
-									f = e.getValue();
-									break;
+						if (mySensor instanceof I2CProc) {
+							String dataList[] = t.get(i).getString("data")
+									.split(";");
+							for (String dataElement : dataList) {
+								String dataVals[] = dataElement.split(":");
+								String register = dataVals[0];
+								double val = Double.parseDouble(dataVals[1]);
+								f = null;
+								for (Map.Entry<String, Formula> e : mySensor
+										.getFormulaContainer().getFc()
+										.entrySet()) {
+									if (e.getValue().getDisplayName()
+											.equals(register)) {
+										f = e.getValue();
+										break;
+									}
 								}
+								f.setValue(val);
 							}
+
+							// checkEmptyRegisters.put(register, false);
+						} else {
+							String dataVals[] = t.get(i).getString("data")
+									.split(";");
+							String dataList[] = t.get(i).getString("data")
+									.split(":");
+							double val = Double.parseDouble(dataList[1]);
+							f = mySensor.getFormulaContainer().getFc()
+									.get("pin");
 							f.setValue(val);
-							checkEmptyRegisters.put(register, false);
-						}else{
-							f = mySensor.getFormulaContainer().getFc().get("pin");
-							String value = t.get(i).getString("data");
-							Double d = Double.parseDouble(value);
-							f.setValue(d);
-							checkEmptyRegisters.put("pin", false);
 						}
-						//When formula evaluation takes place
-						if(!checkEmptyRegisters.containsValue(true)){
-							mySensor.getFormulaContainer().evaluate();
-							L.d("leaving formula container");
-							mySensor.getFormulaContainer().logAllFormulas();
-							String s = "";
-							for(Map.Entry<String, Formula> e : mySensor.getFormulaContainer().getFc().entrySet()){
-								s=e.getValue().getValue()+"";
-							}
-							sensorData = s;
-							displayData(sensorData,date);
-							initializeInputs();
+						// When formula evaluation takes place
+						mySensor.getFormulaContainer().evaluate();
+						L.d("leaving formula container");
+						mySensor.getFormulaContainer().logAllFormulas();
+						String s = "";
+						for (Map.Entry<String, Formula> e : mySensor
+								.getFormulaContainer().getFc().entrySet()) {
+							s = e.getValue().getValue() + "";
 						}
+						sensorData = s;
+						L.d("value after formula evaluation " + sensorData);
+						displayRawData(sensorData, date);
+						displayGraphData(sensorData);
+					} else {
+						String dataVals[] = t.get(i).getString("data")
+								.split(";");
+						String dataList[] = t.get(i).getString("data")
+								.split(":");
+						String value = dataList[1];
+						sensorData = value;
+						L.d("rawdata " + sensorData);
+						displayRawData(sensorData, date);
 					}
-					else{
-					String value = t.get(i).getString("data");
-					sensorData = value;
-					displayData(sensorData,date);
-					}
-					L.d(sensorData+" "+date);
+					L.d(sensorData + " " + date);
 				}
-				
+
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -312,40 +329,47 @@ public class DetailsFrag extends Fragment implements ActionBar.TabListener, Load
 		}
 	}
 
-	private void displayData(String sensorData, String date) {
+	private void displayGraphData(String sensorData) {
 		// TODO Auto-generated method stub
-		if(info==null && infoAdapter==null){
-			L.d("info or infoAdapter null hain");
-		}
-		if(series == null){
-			L.d("Series null");
-		}
-		//notify the SensorList activity that a value has been recieved
-		info.add(mySensor.getId()+":"+sensorData+" Time:"+date);
-		infoAdapter.notifyDataSetChanged();
 		final double graphVal = Double.parseDouble(sensorData);
 		getActivity().runOnUiThread(new Runnable() {
-              @Override
-              public void run() {
-            	  //keep appending the particular list
-            	  JSONObject obj = mySensor.getJSON();
-                  try {
-					series.appendData(new GraphView.GraphViewData(TIME + (float)(obj.getInt("rate")), graphVal), true);
+			@Override
+			public void run() {
+				// keep appending the particular list
+				JSONObject obj = mySensor.getJSON();
+				try {
+					// TODO change hardcoded value
+					series.appendData(new GraphView.GraphViewData(TIME
+							+ (float) (1000), graphVal), true);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
-					series.appendData(new GraphView.GraphViewData(TIME ++, graphVal), true);
+					series.appendData(new GraphView.GraphViewData(TIME++,
+							graphVal), true);
 					e.printStackTrace();
 				}
-                  //Here I try few different things
-                  
-              }
-          });
+				// Here I try few different things
+
+			}
+		});
+	}
+
+	private void displayRawData(String sensorData, String date) {
+		// TODO Auto-generated method stub
+		if (info == null && infoAdapter == null) {
+			L.d("info or infoAdapter null hain");
+		}
+		if (series == null) {
+			L.d("Series null");
+		}
+		// notify the SensorList activity that a value has been recieved
+		info.add(mySensor.getId() + ":" + sensorData + " Time:" + date);
+		infoAdapter.notifyDataSetChanged();
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Void> arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -355,14 +379,13 @@ public class DetailsFrag extends Fragment implements ActionBar.TabListener, Load
 		mAdapter = null;
 		super.onDestroy();
 	}
-	
-	
 
-//	@Override
-//	public void sendArrayWithAdapter(ArrayAdapter<String> a,ArrayList<String> data) {
-//		// TODO Auto-generated method stub
-//		detailAdapter = a;
-//		info = data;
-//	}
+	// @Override
+	// public void sendArrayWithAdapter(ArrayAdapter<String> a,ArrayList<String>
+	// data) {
+	// // TODO Auto-generated method stub
+	// detailAdapter = a;
+	// info = data;
+	// }
 
 }
